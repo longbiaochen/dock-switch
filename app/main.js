e27a6975dc4b8d90b141e023846f14ed8e3117e2
Longbiao CHEN <longbiaochen@icloud.com>
2019-06-09 14:18:58 +0800

    Quick commit at 2019-06-09-14:18:58

--- app/main.js
+++ app/main.js
@@ -32,7 +32,7 @@ electron.app.on("ready", () => {
         if (electron.win.isVisible()) {
             electron.win.hide();
         } else {
-            var response = child_process.execSync(`${__dirname}/bin/dock`).toString();
+            var response = child_process.execSync(`${__dirname}/ui-helper dock 0`).toString();
             // console.log(response);
             dock_items = JSON.parse(response);
             show_window();
