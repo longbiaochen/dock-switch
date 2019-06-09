71f53a967f22c3cf1b70fec3647e13824a82100e
Longbiao CHEN <longbiaochen@icloud.com>
2019-06-09 13:20:24 +0800

    Quick commit at 2019-06-09-13:20:24

---
+++ axuielement/index.js
@@ -0,0 +1,11 @@
+exports.dock = function() {
+    console.log("dock");
+}
+
+exports.window = function(id) {
+    console.log("window: " + id);
+}
+
+exports.mouse = function(id) {
+    console.log("mouse: " + id);
+}
