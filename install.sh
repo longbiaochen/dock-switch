c6359cf175d38b07444c583a2e4b5d297d7c3e81
Longbiao CHEN <longbiaochen@icloud.com>
2019-08-25 08:19:04 +0800

    the first commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+electron-builder
+killall -9 Electron
+killall -9 dock-switch
+rm -rf /Applications/dock-switch.app/
+cp -rf dist/mac/dock-switch.app /Applications/dock-switch.app
+open -a dock-switch
