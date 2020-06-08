f678c235083d0ddf0843e647bc52633ab484a4db
Longbiao CHEN <longbiaochen@icloud.com>
2020-06-08 08:49:44 +0800

    quick commit

--- app/index.js
+++ app/index.js
@@ -7,12 +7,13 @@ const child_process = require("child_process");
 var CONFIG = require(`${__dirname}/config.json`);
 var ITEM_TPL = `<div class="item" style="left: %dpx; top: 0;""><button type="button" class="btn btn-info">%s</button></div>`;
 var APP_TPL = `open -a "%s"; sleep .1;`;
-var SCREEN_TPL = `${__dirname}/ui-helper screen %s`;
-var KEY_MAP = { 37: "2", 38: "0", 39: "1"};
+var SCREEN_TPL = `ui-helper screen %s`;
+var KEY_MAP = { 37: "2", 38: "0", 39: "1" };
 var ITEMS = [];
 
-$(function() {
-<<<<<<< HEAD
+$(function() { <<
+  <<
+  << < HEAD
   $(document).on("keydown", function(e) {
     electron.remote.app.hide();
     // new Notification('Title', { body: e.keyCode });
@@ -43,43 +44,6 @@ $(function() {
           name: dock_items[i].name,
           key: k++,
           screen: ""
-=======
-    $(document).on("keydown", function(e) {
-        electron.remote.app.hide();
-        // new Notification('Title', { body: e.keyCode });
-        var screen = electron.screen.getAllDisplays();
-        if (KEY_MAP[e.keyCode] != undefined) {
-            // Moves
-            var name = child_process.execSync(util.format(SCREEN_TPL, KEY_MAP[e.keyCode])).toString();
-            // new Notification(KEY_MAP[e.keyCode], { body: name });
-            var item = ITEMS.find(item => item.name == name);
-            item.screen = KEY_MAP[e.keyCode];
-        } else {
-            // Apps
-            var key = e.key.toUpperCase();
-            var item = ITEMS.find(item => item.key == key);
-            // new Notification(item.screen, { body: e.keyCode });
-            child_process.execSync(util.format(APP_TPL, item.name));
-            child_process.execSync(util.format(SCREEN_TPL, item.screen));
-        }
-    });
-
-    electron.ipcRenderer.on("update-ui", (event, dock_items) => {
-        $("#container").html("");
-        ITEMS = [];
-        var k = 1;
-        for (var i = 0; i < dock_items.length - 3; i++) {
-            var item = CONFIG.items.find(item => item.name == dock_items[i].name);
-            if (item == undefined) {
-                item = {
-                    name: dock_items[i].name,
-                    key: k++,
-                    screen: ""
-                }
-            }
-            ITEMS.push(item);
-            $("#container").append(util.format(ITEM_TPL, i * 52, item.icon ? item.icon : item.key));
->>>>>>> 6d0a6e52aa202a62cd2237ec2b050c5a11d08dec
         }
       }
       ITEMS.push(item);
--- install.sh
+++
@@ -1,6 +0,0 @@
-yarn dist
-killall -9 Electron
-killall -9 dock-switch
-rm -rf /Applications/dock-switch.app/
-cp -rf dist/mac/dock-switch.app /Applications/dock-switch.app
-open -a dock-switch
